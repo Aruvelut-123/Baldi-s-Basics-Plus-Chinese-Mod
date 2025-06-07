3770338d109ae938e2368ade2ed6b3c225616637
Baymaxawa <2502474438@qq.com>
2025-06-07 19:59:17 +0800

    more updates

--- BBPC/Plugin.cs
+++ BBPC/Plugin.cs
@@ -2,8 +2,14 @@
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
+using MTM101BaldAPI;
+using MTM101BaldAPI.AssetTools;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -16,25 +22,38 @@ using UnityEngine.SceneManagement;
  */
 namespace BBPC
 {
-    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, "0.0.0.3")]
+    [BepInDependency("mtm101.rulerp.bbplus.baldidevapi", BepInDependency.DependencyFlags.HardDependency)]
+
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, "0.0.0.4")]
     public class Plugin : BaseUnityPlugin
     {
+        static string _modPath = string.Empty;
+        public static string ModPath => _modPath;
         internal static new ManualLogSource Logger;
         private Harmony harmony;
         private ConfigFile config;
         private TextMeshProUGUI versionLabel;
         private Watermark watermarkGO;
         private GameObject watermark;
+        public static ConfigEntry<bool> disable_credits;
         private ConfigEntry<bool> is_dev;
         private ConfigEntry<bool> is_beta;
         private ConfigEntry<bool> is_alpha;
         private ConfigEntry<string> version;
+        private Credit credit_handler;
 
         private void Awake()
         {
+            _modPath = AssetLoader.GetModPath(this);
             config = new ConfigFile(
                 Path.Combine(Paths.ConfigPath, MyPluginInfo.PLUGIN_GUID+".cfg"),
                 true
+            );
+            disable_credits = config.Bind(
+                "Debugging",
+                "disable_credits",
+                false,
+                "Disable Credits 'Press any key will go back' Function"
             );
             is_dev = config.Bind(
                 "Build Check",
@@ -62,6 +81,7 @@ namespace BBPC
             );
             config.Save();
             Logger = base.Logger;
+            credit_handler = new Credit(this);
             harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
             harmony.PatchAll();
             watermarkGO = new Watermark(is_dev.Value, is_alpha.Value, is_beta.Value, this);
@@ -116,6 +136,8 @@ namespace BBPC
             }
             try
             {
+                GameObject versionLabelGO = GameObject.Find("Reminder");
+                if (versionLabelGO == null) return;
                 UpdateVersionLabel();
                 CreateWatermark();
             }
@@ -173,7 +195,7 @@ namespace BBPC
         {
             if (SceneManager.GetActiveScene().name != "Credits") return;
 
-            GameObject extra = GameObject.Find("ExtraCreditsScreen");
+            GameObject extra = GameObject.Find("ExtraCreditsScreen(0)");
             GameObject allBackers = GameObject.Find("All Backers");
 
             if (extra != null && allBackers != null)
@@ -194,27 +216,6 @@ namespace BBPC
             Logger.LogWarning(msg);
         }
 
-        //Use these when debugging, it disables return to main menu function in credits screen.
-        /*[HarmonyPatch(typeof(Credits), "Update")]
-        public class Credits_Update_DisableReturnPatch
-        {
-            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-            {
-                var codes = new List<CodeInstruction>(instructions);
-                for (int i = 0; i < codes.Count; i++)
-                {
-                    if (codes[i].opcode == OpCodes.Ldstr && (string)codes[i].operand == "MainMenu")
-                    {
-                        Logger.LogDebug("Found MainMenu load instruction; replacing with NOPs.");
-                        codes[i].opcode = OpCodes.Nop;
-                        if (i + 1 < codes.Count) codes[i + 1].opcode = OpCodes.Nop;
-                        if (i + 2 < codes.Count) codes[i + 2].opcode = OpCodes.Nop;
-                    }
-                }
-                return codes.AsEnumerable();
-            }
-        }*/
-
         [HarmonyPatch(typeof(Credits), "Start")]
         public class Credits_Start_Patch
         {
@@ -222,25 +223,71 @@ namespace BBPC
             {
                 if (__instance.screens != null && __instance.screens.Count > 0)
                 {
+                    JObject json = Credit.credit_json;
+                    int i = 0;
+                    int c = 1;
                     GameObject originalScreen = __instance.screens[0].gameObject;
-                    GameObject clonedScreen = Instantiate(originalScreen, originalScreen.transform.parent);
-                    clonedScreen.name = "ExtraCreditsScreen";
-                    clonedScreen.SetActive(false);
-                    TMP_Text[] texts = clonedScreen.GetComponentsInChildren<TMP_Text>();
-                    if (texts != null && texts.Length > 0)
+                    foreach (JObject page in json["pages"])
                     {
-                        TMP_Text mainText = texts[0];
-                        for (int i = 1; i < texts.Length; i++)
+                        GameObject clonedScreen = Instantiate(originalScreen, originalScreen.transform.parent);
+                        clonedScreen.name = "ExtraCreditsScreen(" + i.ToString() + ")";
+                        clonedScreen.SetActive(false);
+                        TMP_Text[] texts = clonedScreen.GetComponentsInChildren<TMP_Text>();
+                        if (texts != null && texts.Length > 0)
                         {
-                            Destroy(texts[i].gameObject);
-                        }
-                        mainText.text = "<b>BB+汉化模组</b>\n\n主要汉化人员:\nBaymaxawa & MEMZSystem32\n\n润色: 馒\n\nTMP字体: CMCGZP\n\n特别鸣谢: ChatGPT\n\n<size=12>感谢所有在群内参与测试和提供的人员! 没有你们很难做到这里!</size>";
-                        mainText.fontSize = 24;
-                        mainText.alignment = TextAlignmentOptions.Center;
-                        mainText.color = Color.white;
+                            TMP_Text mainText = texts[0];
+                            Destroy(texts[1]);
+                            string text_screen = "";
+                            string sponser = "";
+                            foreach (string text in Credit.sponsers)
+                            {
+                                sponser += text + "\n";
+                            }
+                            foreach (JToken text in page["text"])
+                            {
+                                if (text.ToString().Contains("{AFDIAN_SPONSERS}"))
+                                {
+                                    string text2 = sponser;
+                                    text_screen += text2 + "\n";
+                                }
+                                else
+                                {
+                                    text_screen += text.ToString() + "\n";
+                                }
+                            }
+                            mainText.text = text_screen;
+                            mainText.fontSize = 24;
+                            mainText.alignment = TextAlignmentOptions.Center;
+                            mainText.color = Color.white;
+                        } 
+                        __instance.screens.Insert(c, clonedScreen.GetComponent<Canvas>());
+                        i++;
+                        c++;
                     }
-                    __instance.screens.Insert(1, clonedScreen.GetComponent<Canvas>());
                 }
+            }
+        }
+
+        [HarmonyPatch(typeof(Credits), "Update")]
+        public class Credits_Update_DisableReturnPatch
+        {
+            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codes = new List<CodeInstruction>(instructions);
+                if (Plugin.disable_credits.Value)
+                {
+                    for (int i = 0; i < codes.Count; i++)
+                    {
+                        if (codes[i].opcode == OpCodes.Ldstr && (string)codes[i].operand == "MainMenu")
+                        {
+                            Logger.LogDebug("Found MainMenu load instruction; replacing with NOPs.");
+                            codes[i].opcode = OpCodes.Nop;
+                            if (i + 1 < codes.Count) codes[i + 1].opcode = OpCodes.Nop;
+                            if (i + 2 < codes.Count) codes[i + 2].opcode = OpCodes.Nop;
+                        }
+                    }
+                }
+                return codes.AsEnumerable();
             }
         }
     }
