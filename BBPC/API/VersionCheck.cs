8ce5d138bd35c9469fad5824f1293df32eb04e97
Baymaxawa <2502474438@qq.com>
2026-03-28 19:58:33 +0800

    fix: log output in VersionCheck system feat: update expectedGameVersions

--- BBPC/API/VersionCheck.cs
+++ BBPC/API/VersionCheck.cs
@@ -8,16 +8,24 @@ namespace BBPC.API
         public static bool CheckGameVersion(string[] expectedVersions, PluginInfo info)
         {
             bool is_supported = false;
+            string versions = "";
+            int i = 0;
             foreach (string expectedVersion in expectedVersions) {
                 if (Application.version == expectedVersion)
                 {
                     is_supported = true;
                     break;
                 }
+                else
+                {
+                    if (i < expectedVersions.Length - 1) versions += expectedVersion + " ";
+                    else versions += expectedVersion;
+                }
+                i++;
             }
             if (!is_supported)
             {
-                string errorMessage = $"游戏版本 ({Application.version}) 与要求的版本 ({expectedVersions.ToString()}) 不匹配。模组可能无法正常工作。";
+                string errorMessage = $"游戏版本 ({Application.version}) 与要求的版本 ({versions}) 不匹配。模组可能无法正常工作。";
                 API.Logger.Error(errorMessage);
             }
             return is_supported;
